02398b881f10cfc2d8653e194624c4ccf39b80d5
marky <mark.soriano04@yahoo.com>
2024-06-18 09:07:36 +0800

    Initial commit

---
+++ src/main/java/gazeupon/magiclectern/MagicLecternClient.java
@@ -0,0 +1,10 @@
+package gazeupon.magiclectern;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MagicLecternClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
